3d13744df9f22535e198aa4e6ff0c6e5d1853d21
Achi3v3d <udokwu.maduabuchi@gmail.com>
2021-12-30 14:48:20 -0500

    almost done

--- app/src/main/java/com/example/musicassignment2/MainActivity.kt
+++ app/src/main/java/com/example/musicassignment2/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.musicassignment2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.musicassignment2.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    /**
+     * In on onCreate Method of main activity we bind the adapter for the 3 fragments
+     * and we define de position of them
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
     }
 }
